a8c70aad4e0988cf64a1036768c1a8147d407e7d
loudeco <louise.decombe@laplateforme.io>
2021-03-19 14:42:31 +0100

    first commit

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig');
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+
+    public function home(){
+        return $this->render('blog/home.html.twig');
+    }
+
+}
